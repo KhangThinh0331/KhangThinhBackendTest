353b13d4729119ef8e3946a4a9100ef64131fd7a
KhangThinh0331 <khangthinh0331@gmail.com>
2026-01-08 10:34:33 +0700

    fix some logics

--- src/delta.js
+++ src/delta.js
@@ -1,15 +1,17 @@
 const fs = require("fs");
+const path = require("path")
 
-const STATE_FILE = "state.json";
+const STATE_FILE = path.join(__dirname, "..", "state.json");
 
 function loadState() {
   if (!fs.existsSync(STATE_FILE)) return {};
-  return JSON.parse(fs.readFileSync(STATE_FILE));
+  return JSON.parse(fs.readFileSync(STATE_FILE, "utf8"));
 }
 
 function saveState(state) {
   fs.writeFileSync(STATE_FILE, JSON.stringify(state, null, 2));
 }
+
 
 function detectDelta(articles) {
   const oldState = loadState();
